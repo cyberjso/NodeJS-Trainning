7126bcec62e5de4f05a006aa65781baa75546b7c
Jackson <cyber.jso@gmail.com>
2014-03-20 12:33:59 -0300

    Fist commit

---
+++ main.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+
+app.get('/hello.txt', function(req, res){
+  res.send('Hello World');
+});
+
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+})
+
